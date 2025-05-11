30d382264a0fa1214fb103ce798d01e48a849f35
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2025-05-12 01:32:06 +0530

    nth

--- app/dashboard/_components/PricingPlan.jsx
+++ app/dashboard/_components/PricingPlan.jsx
@@ -1,14 +1,14 @@
 export default[
     {
-        link:'https://buy.stripe.com/test_9AQ4jI8fffk75igcMN',
+        link:'https://buy.stripe.com/test_00w4gzdZM0qV7HvbgN53O00',
         price:7.99,
-        priceId:'price_1PUNYmSISOkj08jR30L0UpyY',
+        priceId:'price_1RNfwoDR8rWHnLWcYoadcR6G',
         duration:'Monthly'
     },
     {
-        link:'https://buy.stripe.com/test_eVabMa8fffk78us000',
+        link:'https://buy.stripe.com/test_5kQaEXcVIflP1j7esZ53O01',
         price:49.00,
-        priceId:'price_1PUNZQSISOkj08jRFQkKuzXi',
+        priceId:'price_1RNfxmDR8rWHnLWchWJGLZ6q',
         duration:'Yearly'
     }
 ]
